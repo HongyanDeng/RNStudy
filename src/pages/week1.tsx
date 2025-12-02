f3600f50772f597aa8093df04ce36f5a06e1812a
HongyanDeng <denghongyan48@gmail.com>
2025-12-02 17:57:26 +0800

    commit before ts-practice

---
+++ src/pages/week1.tsx
@@ -0,0 +1,47 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type { PropsWithChildren } from 'react';
+import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+} from 'react-native';
+
+import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+    Title: {
+        margin: 100,
+    }
+
+
+})
+
+function App() {
+    return (
+        <View>
+            <Text style={styles.Title}>
+                RNStudy项目
+            </Text>
+        </View>
+    )
+
+}
+
+export default App;
